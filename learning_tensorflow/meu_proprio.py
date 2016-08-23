3cd7ff25d7531e8db78b6cdc7428d59835e9682f
Diego <diego.lima@lais.huol.ufrn.br>
2016-08-23 18:46:07 -0300

    minor changes

--- learning_tensorflow/meu_proprio.py
+++ learning_tensorflow/meu_proprio.py
@@ -1,17 +1,23 @@
+# -*- coding:utf-8 -*-
 from my_functions import *
 from math import log
 
 def centroids_number(samples_number):
-    n_centroids = 1 + (3.3 * log(samples_number, 10))
+    n_centroids = 1 + (3.3 * log(samples_number, 60))
     print n_centroids
     return int(n_centroids)
 
-SAMPLE = create_samples(160,2)
+def iterations_number(samples_number):
+    n_centroids = 1 + (3.3 * log(samples_number, 10))
+    print 'iterações:',n_centroids
+    return int(n_centroids)
+
+SAMPLE = create_samples(320,2)
 
 CENTROIDS = choose_random_centroids(SAMPLE, centroids_number(len(SAMPLE)))
 plot_initial_centroids(CENTROIDS)
 
-for x in range(centroids_number(len(SAMPLE))):
+for x in range(iterations_number(len(SAMPLE))):
     nearest_indices = assign_to_nearest(SAMPLE, CENTROIDS)
     CENTROIDS = update_centroids(SAMPLE,nearest_indices,len(CENTROIDS))
 
