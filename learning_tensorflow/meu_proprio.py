0ca3834ecf070c17d6e043afff8d234d77c134a8
Diego <diego.lima@lais.huol.ufrn.br>
2016-08-23 18:32:53 -0300

    numero de centroides e iteracoes adaptavel

--- learning_tensorflow/meu_proprio.py
+++ learning_tensorflow/meu_proprio.py
@@ -1,15 +1,19 @@
 from my_functions import *
+from math import log
 
-SAMPLE = create_samples(60,2)
+def centroids_number(samples_number):
+    n_centroids = 1 + (3.3 * log(samples_number, 10))
+    print n_centroids
+    return int(n_centroids)
 
-CENTROIDS = choose_random_centroids(SAMPLE, 2)
+SAMPLE = create_samples(160,2)
+
+CENTROIDS = choose_random_centroids(SAMPLE, centroids_number(len(SAMPLE)))
 plot_initial_centroids(CENTROIDS)
 
-for x in range(3):
+for x in range(centroids_number(len(SAMPLE))):
     nearest_indices = assign_to_nearest(SAMPLE, CENTROIDS)
     CENTROIDS = update_centroids(SAMPLE,nearest_indices,len(CENTROIDS))
-    print 'updated:'
-    print CENTROIDS
 
     plot_sample(SAMPLE)
     plot_centroids(CENTROIDS)
