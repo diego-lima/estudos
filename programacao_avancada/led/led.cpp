fe9d8d4c5a75c113232f8ad1253734cfeb86c49f
Diego Lima <diego.lima@lais.huol.ufrn.br>
2018-04-03 18:14:57 -0300

    transposta

---
+++ programacao_avancada/led/led.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+float* transposta(float* x, int nl, int nc){
+    float *y;
+    int i,j;
+    y = (float*) malloc(nl*nc*sizeof(float));
+
+    memcpy(y, x, nl*nc*sizeof(float));
+
+    for(i = 0; i < nl; i++){
+        for(j = 0; j < nc; j++){
+            x[i*nc + j] = y[j*nc + i];
+        }
+    }
+
+}
+
+int main(){
+    float *A;
+    int nl=2, nc=3, i;
+
+    // 
+    // MATRIZ A
+    // 
+    // Alocar
+    A = (float*) malloc(nl*nc*sizeof(float));
+
+    // Preencher
+    for(i=0; i<nl*nc; i++){
+        A[i] = i;
+    }
+
+
+    // Printar
+    for(i=0; i<nl*nc; i++){
+        printf("%.0f ", A[i]);
+        if ( (i+1) % nc == 0){
+            printf("\n");
+        }
+    }
+    printf("\n");
+
+    transposta(A, nl, nc);
+
+    // Printar
+    for(i=0; i<nl*nc; i++){
+        printf("%.0f ", A[i]);
+        if ( (i+1) % nl == 0){
+            printf("\n");
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
