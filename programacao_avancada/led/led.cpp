6682122d90ee623f85727b3b89fe1b6704c8844d
Diego Lima <ddddiegolima@gmail.com>
2018-04-03 20:25:00 -0300

    conversão de coluna em char com sucesso

--- programacao_avancada/led/led.cpp
+++ programacao_avancada/led/led.cpp
@@ -4,6 +4,17 @@
 
 const int NC = 8; // numero de colunas
 
+int comparativa(char a, char b){
+  int pot;
+  for(int i=0; i<8; i++){
+    pot = (int) pow(2,i);
+    if( (pot & a) != (pot & b) ){
+      return 0;
+    }
+  }
+  return 1;
+}
+
 unsigned char prepara(char* coluna){
 
     int acumulador = 0; // vamos acumular aqui as somas das potencias de 2 para no final ter um numero decimal
@@ -11,29 +22,29 @@ unsigned char prepara(char* coluna){
     
 
     for(int i=0; i<NC; i++){
-        if(comparativa(coluna[i],'0')) acumulador = acumulador + pow(2,i); // se, naquela coluna tem 1, entao o bit daquela potencia eh 1
+        if(comparativa(coluna[i],'1')){
+            acumulador = acumulador + pow(2,i); // se, naquela coluna tem 1, entao o bit daquela potencia eh 1
+        }
     }
+
+    printf("acumulador: %d\n", acumulador);
+
+    bits = acumulador;
     return bits;
 }
 
-int comparativa(char a, char b){
-  int pot;
-  for(int i=0; i<8; i++){
-    pot = (int) pow(2,i);
-    if(pot & a != pot & b){
-      return 0;
-    }
-  }
-  return 1;
-}
-
 int main(){
+    char coluna[] = {'0','0','0','0','0','0','1','0'};
+    unsigned char bits;
+    // Printando alguns testes
+    printf("----ASSERTS----\n");
+    printf("'1' comparativa '1': %d\n", comparativa('1', '1'));
+    printf("coluna[0] comparativa '1': %d\n", comparativa(coluna[0], '1'));
+    printf("----ASSERTS----\n\n");
 
-  char a = '0', b = '0';
-
-  int r = comparativa(a,b);
-
-  printf("%d\n", r);
-
-  return 0;
+    // Executando o programa realmente
+    bits = prepara(coluna);
+    printf("bits: %c\n", bits);
+    
+    return 0;
 }
