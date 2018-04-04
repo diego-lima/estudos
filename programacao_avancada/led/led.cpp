17603c776d44cd6a7e1e7a4be9e8f4c8d581ebe2
Diego Lima <ddddiegolima@gmail.com>
2018-04-03 21:28:45 -0300

    comentarios

--- programacao_avancada/led/led.cpp
+++ programacao_avancada/led/led.cpp
@@ -3,10 +3,15 @@
 #include <math.h>
 
 const int NC = 8; // numero de colunas
+const int N_BITS=8; // bits em um byte
 
 int comparativa(char a, char b){
+    /*
+    Essa funcao recebe dois chars, e verifica se seus bits sao todos iguais.
+    Se sim, retorna 1
+    Se nao, retorna 0
+    */
   int pot; // potencias
-  int N_BITS=8;
   for(int i=0; i<N_BITS; i++){
     pot = (int) pow(2,i);
     if( (pot & a) != (pot & b) ){
@@ -19,7 +24,7 @@ int comparativa(char a, char b){
 unsigned char prepara(char* coluna){
     /*
     Recebe um vetor de char, preeenchido com '0' e '1'
-    O retorno é a sequencia correspondente de bits, convertido para char.
+    O retorno é a sequencia correspondente de bits convertida para char.
     A leitura do vetor é no sentido do LSB na primeira posicao para o MSB na ultima.
     */
 
@@ -38,11 +43,17 @@ unsigned char prepara(char* coluna){
 }
 
 char *reconstroi(char *vetor, char bits){
-    int extrator_bits;
-    for (int i = 0; i < NC; i++){
-        extrator_bits = pow(2,i);
+    /*
+    Recebe um vetor, onde sera feita a reconstrucao
+    Recebe um char, de onde serao extraidos os bits
+    O primeiro elemento do vetor correspondera ao LSB do char
+    O ultimo elemento do vetor correspondera ao MSB do char
+    */
+    int extrator_bits; // faremos 'e lógico' com potencias de 2 para extrair bit de cada 'casa'
+    for (int i = 0; i < N_BITS; i++){
+        extrator_bits = pow(2,i); // potencia de 2
         
-        if ( (bits & extrator_bits) != 0)
+        if ( (bits & extrator_bits) != 0) //as potencias de 2 tem um '1' em apenas uma 'casa'
             vetor[i] = '1';
         else
             vetor[i] = '0';
@@ -64,14 +75,16 @@ int main(){
     // 
     // TRANSMISSOR
     // 
+
     printf("coluna:              %.8s\n", coluna); // printando o vetor de char, somente 8 caracteres
     bits = prepara(coluna);                        //comprimindo e guardando num char apenas
     printf("bits: %c\n", bits);
-    // bits seria enviado
+    // bits seria enviado de alguma forma
     
     // 
     // RECEPTOR
     // 
+
     // bits é recebido de algum lugar
     reconstroi(coluna_recebida, bits);
     printf("coluna recebida:     %.8s\n", coluna_recebida);
